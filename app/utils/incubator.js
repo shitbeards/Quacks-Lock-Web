46d235f572e531c318e6d5eec280c2725a08efed
jamesstidard <jamesstidard@gmail.com>
2016-04-02 17:40:43 +0100

    fix: index bounds

--- app/utils/incubator.js
+++ app/utils/incubator.js
@@ -1,13 +1,13 @@
 import 'seedrandom'
 
 function random(max) {
-    Math.round(Math.random() * max) + 1
+    Math.round(Math.random() * max)
 }
 
-export function hatch(quack_code, skins=[], voices=[]) {
-    Math.seedrandom(quack_code)  // Seed Math.random() for deterministic values
-    const skin  = skins[random(skins.length)]
-    const voice = voices[random(voices.length)]
+export function hatch(quack_seed, skins=[], quacks=[]) {
+    Math.seedrandom(quack_seed)  // Seed Math.random() for deterministic values
+    const skin  =  skins[random( skins.length - 1)]
+    const quack = quacks[random(quacks.length - 1)]
     Math.seedrandom()  // Return Math.random() back to normal
-    return [skin, voice]
+    return [skin, quack]
 }
