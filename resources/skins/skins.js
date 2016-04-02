77569d257227f8df189909e388876fa5ed4ebf9e
Goatus123 <oskar@spddo.co.uk>
2016-04-02 20:45:36 +0100

    skins

--- resources/skins/skins.js
+++ resources/skins/skins.js
@@ -1,5 +1,13 @@
-// import boba_duck from './boba-duck.svg!text';
+import boba_duck from './boba_duck.svg!text';
+import goose from './goose.svg!text';
+import hunt from './hunt.svg!text';
+import psyduck from './psyduck.svg!text';
+import six from './six.svg!text';
 
 export default [
-    "<div></div>"
+    boba_duck,
+    goose,
+    hunt,
+    psyduck,
+    six
 ]
